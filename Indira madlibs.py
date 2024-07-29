c5f8c8d77dcf7316f3b9d017a1385574f285f90a
Indira Venkiteshwaran <penguin2500coder@penguincoding.org>
2024-07-29 14:50:17 -0400

    Add files via upload

---
+++ Indira madlibs.py
@@ -0,0 +1,23 @@
+print("earth")
+print("blue")
+name=input("what is your name")
+print("hi "+ name)
+color=input("what is your favorite color")
+
+
+verb=input("enter a verb ")
+place= input("Enter a place")
+print("tommorow you will" +verb " to the" + place)
+
+
+noun=input("enter a noun")
+verb=input("enter a verb")
+print("the" +noun "went" +verb)
+
+food=input("enter a food")
+verb=input("enter a verb")
+print("I love" +food "when it goes" +verb)
+
+place=input("enter a place")
+noun=input("enter a noun")
+print("today we went to" +place "and saw" +noun)
---
+++ quiz.py
@@ -0,0 +1,31 @@
+age=int(input("what is your age"))
+if age>16:
+    print("yes drive")
+else:
+    print("no drive")
+
+
+color=input("What is your favorite color")
+if color==("blue"):
+    print("Me too")
+else:
+    print("I like that too")
+
+
+
+
+
+queshtion1=input("Which country drinks the most coffe")
+if queshtion1=="finland":
+    print("correct")
+else:
+    print("incorrect,it is finland")
+
+
+queshtion2=input("Whart is the birthstone for june")
+if queshtion2=="pearl":
+    print("correct")
+else:
+    print("wrong,the answer is pearl")
+
+
